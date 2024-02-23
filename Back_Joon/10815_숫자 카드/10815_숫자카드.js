6318892c0e9ec26b1541bd5e8100f00ac9b5737d
criticbanggu@gmail.com <criticbanggu@gmail.com>
2024-02-24 04:51:41 +0900

    (BOJ) 10815_숫자 카드

---
+++ Back_Joon/10815_숫자 카드/10815_숫자카드.js
@@ -0,0 +1,26 @@
+// const fs = require("fs");
+// function solution(input) {
+//   console.log(input);
+// }
+
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+// solution(input);
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = [+input[0], +input[2]];
+const [hasCards, cards] = [
+  new Set(input[1].split(" ").map(Number)),
+  input[3].split(" ").map(Number),
+];
+
+let answer = "";
+for (let i = 0; i < M; i++) {
+  if (hasCards.has(cards[i])) answer += 1 + " ";
+  else answer += 0 + " ";
+}
+
+console.log(answer);
