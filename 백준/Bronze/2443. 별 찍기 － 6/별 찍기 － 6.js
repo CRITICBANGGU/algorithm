ff6471470a7c781b12aea633d2575dbe229bf1c1
BANG JAEHYUN <95038747+CRITICBANGGU@users.noreply.github.com>
2024-07-30 15:50:05 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 136 ms, Memory: 10136 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim();
+
+function main(str) {
+    var idx = Number(str);
+    for (let i = 1; i <= idx; i++) {
+        var result = '';
+        for (let j = 1; j < i; j++) {
+            result += ' ';
+        }
+        for (let k = 0; k <= idx - i; k++) {
+            result += '*'
+        }
+        for (let p = 0; p < idx - i; p++) {
+            result += '*'
+        }
+        console.log(result)
+    }
+}
+
+main(input);
